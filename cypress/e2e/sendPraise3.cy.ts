fde5b9e00b00910ce2f2f77eb61268b42c0144ad
MarcinCyganNMwIT <cygan.marcin89@outlook.com>
2024-12-01 00:08:27 +0100

    sendPraise3 test change

--- cypress/e2e/sendPraise3.cy.ts
+++ cypress/e2e/sendPraise3.cy.ts
@@ -1,40 +1,72 @@
 describe("Sending praise", () => {
     beforeEach(() => {
+
       cy.visit("/");
-  
+
       cy.login({ login: "user8", password: "user8pass" });
-  
-      cy.clearPraisesIfNeeded();
+
     });
   
     it("Test of sending praise to another user", () => {
-
+      
+        cy.intercept('GET', '/api/praise/competences?size=1000').as('getCompetences');
+        cy.intercept('GET', '/api/praise/projects?size=1000').as('getProjects');
+        cy.intercept('GET', '/api/users?search=Kaczmarek%20Tomasz&size=1000').as('getUser');
+        cy.clearPraisesIfNeeded();
+        cy.wait('@getCompetences');
+        cy.wait('@getProjects');
+      
+      cy.fixture('testData3').then((data) => {
+        
         cy.get("#receiver-select").click();
-        cy.get('input[role="search"]').should("be.visible").type("Tomasz Kaczmarek");
-        cy.get("#681821ea-372f-457b-b1a6-f411d9b7b6d3").should("be.visible");
-        cy.contains("span", "Kaczmarek Tomasz Architekt Systemowy").click();
-        cy.get("#receiver-select").should("have.value", "Kaczmarek Tomasz Architekt Systemowy");
-  
+        cy.get('input[role="search"]').should("be.visible").type(data.receiver);
+        cy.wait('@getUser');
+        cy.get("div[role='listbox']").should("be.visible");
+        cy.contains("span", data.receiver).click();
+
         cy.get("#project-select").click();
-        cy.get('input[role="search"]').should("be.visible").type("Ekosystem e-usług 2.0");
+        cy.get('input[role="search"]').should("be.visible").type(data.project);
         cy.get('div[role="listbox"]').should("be.visible");
-        cy.contains("span", "Ekosystem e-usług 2.0").click();
-        cy.get("#project-select").should("have.value", "Ekosystem e-usług 2.0");
+        cy.contains("span", data.project).click();
 
         cy.get('#competence-select').click();
-        cy.get('input[role="search"').should("be.visible").type("Zaangażowanie");
+        cy.get('input[role="search"').should("be.visible").type(data.competence);
         cy.get('div[role="listbox"]').should("be.visible");
-        cy.contains("span", "Zaangażowanie").click();
-        cy.get("#competence-select").should("have.value", "Zaangażowanie");
+        cy.contains("span", data.competence).click();
 
-        cy.get('gds-textarea[data-test="give-praise-comment-textarea"] textarea').type("cy.test.best");
+        cy.get('gds-textarea[data-test="give-praise-comment-textarea"] textarea').type(data.comment);
+
+        cy.get("#receiver-select").should("contain.value", data.receiver);
+        cy.get("#project-select").should("have.value", data.project);
+        cy.get("#competence-select").should("have.value", data.competence);
+
+      })
+        
+
 
         cy.intercept('POST', '/api/praise/create').as('sendPraise')
         cy.get("#submit-button").click();
         cy.wait('@sendPraise').then((interception) => {
             expect(interception.response.statusCode).to.eq(201);
+            const praiseId = interception.response.body.id;
+            Cypress.env('praiseId', praiseId);
         })
         cy.get("gds-snackbar div div div").should("contain.text", "Pochwała została wysłana. Wysłanych w tym miesiącu: ");
-    })
+    });
 
-  });
+    it("Check that the praise is on the main board", () => {
+      cy.intercept('POST', '/api/praise/find?page=0&size=10').as('getPraises');
+    
+      const praiseId = Cypress.env('praiseId');
+    
+      cy.wait('@getPraises').then((interception) => {
+        expect(interception.response.statusCode).to.eq(200);
+    
+        const praises = interception.response.body.content;
+        const foundPraise = praises.filter(praise => praise.id === praiseId);
+    
+        expect(foundPraise.length).to.eq(1); 
+        expect(foundPraise[0].id).to.eq(praiseId);
+      });
+    });
+});
