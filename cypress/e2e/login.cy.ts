099466b2e305e7bdbe3a05b6845b7f14c44436f9
Owieczek <112249898+Owieczek@users.noreply.github.com>
2024-12-03 16:36:28 +0100

    feat: changes

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -2,7 +2,6 @@ describe("User Authentication", () => {
   it("should log in successfully", () => {
     cy.visit("/");
 
-    loginPage.login('user8', 'user8pass');
-    cy.url().should("eq", "https://bcnowimy-gcfhebf4cefrchfs.z01.azurefd.net/");
+    cy.login({ login: "user8", password: "user8pass" });
   });
 });
--- cypress/e2e/sendPraise.cy.ts
+++ cypress/e2e/sendPraise.cy.ts
@@ -1,18 +1,3 @@
-
-
-describe("Filling out the praise form", () => {
-  beforeEach(() => {
-    cy.visit("/");
-
-    cy.login();
-
-    cy.clearPraisesIfNeeded();
-  });
-
-  it("Filling out the praise form with correct data", () => {
-    cy.get("#receiver-select").click();
-    cy.get('input[type="text"]').should("be.visible").type("ada")
-
 describe("Filling out the praise form", () => {
   beforeEach(() => {
     cy.fixture("testData").then((data) => {
@@ -21,7 +6,6 @@ describe("Filling out the praise form", () => {
       cy.login();
 
       cy.clearPraisesIfNeeded();
- 5083a2f44a9fcfa1f02f4bc5e09d028c70b6d175
 
       cy.get("#receiver-select").click();
       cy.get('input[type="text"]').type(data.receiver);
@@ -33,13 +17,42 @@ describe("Filling out the praise form", () => {
       cy.get("#competence-select").click();
       cy.contains("span", data.competence).click();
 
-    cy.get("textarea").type("CypressTest");
+      cy.get("textarea").type(data.message);
 
       cy.intercept("POST", "/api/praise/create").as("submitForm");
       cy.get('button[type="submit"]').click();
     });
   });
 
-    cy.wait("@submitForm").its("response.statusCode").should("eq", 201);
+  it("Should submit the praise form and return the correct response", () => {
+    cy.get("@submitForm").its("response.statusCode").should("eq", 201);
+  });
+
+  it("Should show a success message after submission", () => {
+    cy.get(".gds-snackbar__body__message")
+      .should("be.visible")
+      .and("contain", "Pochwała została wysłana.")
+      .then(() => {
+        cy.get(".gds-snackbar__body__message", { timeout: 5000 }).should("not.exist");
+      });
+  });
+
+  it("Check if the newly submitted praise appears on the main board", () => {
+    cy.wait("@submitForm").then((interception) => {
+      cy.wrap(interception.response.body.id).as("createdPraiseId");
+    });
+
+    cy.intercept("POST", "/api/praise/find?page=0&size=10").as("getPraises");
+
+    cy.visit("/");
+
+    cy.wait("@getPraises").then((interception) => {
+      const praiseIdList = interception.response.body.content;
+
+      cy.get("@createdPraiseId").then((createdPraiseId) => {
+        const praiseIdExists = praiseIdList.some((praise: any) => praise.id === createdPraiseId);
+        expect(praiseIdExists).to.be.true;
+      });
+    });
   });
 });
