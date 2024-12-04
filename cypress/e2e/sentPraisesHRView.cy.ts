433ea5654ac91015aab42a809491844bc30cc85f
margareth91 <margerytka@wp.eu>
2024-12-04 17:10:13 +0100

    test for own sent praises - HR view

---
+++ cypress/e2e/sentPraisesHRView.cy.ts
@@ -0,0 +1,106 @@
+describe("Own Sent Praises HR View", () => {
+
+    beforeEach(() => {
+        cy.visit("/");
+        cy.login({ login: "aleksandra.kwiatkowska", password: "aleksandra2024"});
+        cy.visit("/my-profile");
+        cy.intercept("/api/praise/find*").as("getSentPraises");
+        cy.contains("button", " Wysłane ").click();
+        cy.wait(["@getSentPraises"]);
+    });
+
+    it("should load my-profile page", () => {
+        cy.url().should("contains", "/my-profile");
+    });
+
+    it("should contain user profile praise list", () => {
+        cy.get("[data-test='my-profile-praise-list']").should("be.visible");
+    });
+
+    it("should contain praise list title", () => {
+        cy.get("[data-test='praise-title']").should("be.visible")
+            .contains("Twoje pochwały");
+    });
+
+    it("should contain praise send button", () => {
+        cy.get("[data-test='praise-send-button']").should("be.visible");
+    })
+
+    it("should contain praise tabs", () => {
+        cy.get("[data-test='praise-tabs']").should("be.visible");
+    });
+
+    it("praise tabs should contain tablist", () => {
+        cy.get("[data-test='praise-tabs']").within(() => {
+            cy.contains("button", " Otrzymane ");
+            cy.contains("button", " Wysłane ");
+        });
+    });
+
+    it("should display tab for sent praises", () => {
+        cy.get("#tabpanel-2").should("be.visible");
+    })
+
+    it("sent tab should display sent praise card", () => {
+        cy.get("#tabpanel-2").should("be.visible");
+        cy.get("[data-test='sent-praise-item']").should("be.visible");
+    });
+
+    it("sent praise item should contain header, details and footer", () => {
+        cy.get("[data-test='sent-praise-item']").as("praise-item");
+        cy.get("[data-test='praise-item-header']").should("be.visible");
+        cy.get("@praise-item").each(($obj) => {
+            cy.wrap($obj).within(() => {
+                cy.get("[data-test='praise-item-header']").as("praise-item-header");                
+                cy.get("[data-test='praise-details']").as("praise-item-details");                
+                cy.get("[data-test='praise-footer']").as("praise-footer");
+                cy.get("@praise-item-header").should("be.visible");
+                cy.get("@praise-item-details").should("be.visible");
+                cy.get("@praise-footer").should("be.visible");
+            });
+        });
+    });   
+
+    it("sent praise card header should contain competence and created date", () => {
+        cy.get("[data-test='praise-item-header']").as("praise-item-header");
+        cy.get("@praise-item-header").should("be.visible");
+        cy.get("@praise-item-header").each(($obj) => {
+            cy.wrap($obj).within(() => {
+                cy.get("[data-test='praise-competence']").as("praise-competence");               
+                cy.get("[data-test='praise-created-at']").as("praise-created-at");
+                cy.get("@praise-competence").should("be.visible");
+                cy.get("@praise-created-at").should("be.visible");
+            });
+        });          
+    });
+
+    it("sent praise card details should contain images and users", () => {
+        cy.get("[data-test='praise-details']").as("praise-details");
+        cy.get("@praise-details").should("be.visible");
+        cy.get("@praise-details").each(($obj) => {
+            cy.wrap($obj).within(() => {
+                cy.get("[data-test='praise-users']").as("praise-users");
+                cy.get("@praise-users").should("be.visible");
+            });
+        });
+    });
+
+    it("sent praise card footer should contain project and menu", () => {
+        cy.get("[data-test='praise-footer']").as("praise-footer");
+        cy.get("@praise-footer").should("be.visible");
+        cy.get("@praise-footer").each(($obj) => {
+            cy.wrap($obj).within(() => {
+                cy.get("[data-test='praise-project']").as("praise-project");
+                cy.get("#praise-menu").as("praise-menu");
+                cy.get("@praise-project").should("be.visible");
+                cy.get("@praise-menu").should("be.visible");
+            });
+        });
+    });
+
+    it("check sent praise menu option", () => {
+        cy.get("#praise-menu").click();
+        cy.get("[data-test='praise-delete']").should("have.text", " Usuń");
+    });
+    
+});
